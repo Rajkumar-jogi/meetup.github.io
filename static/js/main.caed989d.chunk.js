ce05f6d1d7f5beb70b6624ad5bfba67e6fbdc678
Rajkumar <Rajkumar.developer143@gmail.com>
2024-04-02 12:49:29 +0000

    Updates

---
+++ static/js/main.caed989d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmeetUp=this.webpackJsonpmeetUp||[]).push([[0],{16:function(e,t,n){},23:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n.n(c),o=n(8),s=n.n(o),i=n(11),d=(n(16),n(2)),j=function(){return Object(d.jsx)("h1",{children:"Hello World"})};s.a.render(Object(d.jsx)(r.a.StrictMode,{children:Object(d.jsx)(i.a,{children:Object(d.jsx)(j,{})})}),document.getElementById("root"))}},[[23,1,2]]]);
+//# sourceMappingURL=main.caed989d.chunk.js.map
